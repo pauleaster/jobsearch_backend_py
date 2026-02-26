11dcb40d6cf9621371993d5ba7105f9ebe2cc717
Paul Easter <paul.john.easter@gmail.com>
2026-02-26 17:52:40 +1100

    Pagination added

--- src/models/api_models/valid_job_search_term.py
+++ src/models/api_models/valid_job_search_term.py
@@ -18,11 +18,12 @@ class FilterTermsRequest(BaseModel):
     filter_terms: List[str] = Field(alias="filterTerms")
     current_job: Optional[bool] = Field(None, alias="currentJob")
     applied_job: Optional[bool] = Field(None, alias="appliedJob")
-    
+    skip: int = Field(0, ge=0)
+    limit: int = Field(100, ge=1, le=1000)
     model_config = ConfigDict(populate_by_name=True)
     
     def __str__(self):
-        return f"FilterTerms: [{', '.join(self.filter_terms)}]"
+        return f"FilterTerms: [{', '.join(self.filter_terms)}], Skip: {self.skip}, Limit: {self.limit}"
 
 
 class SearchTermString(BaseModel):
